5a47c10f5e08c1f7f55f8d80ea9eafdf72f27242
vumg <ab17100222@gmail.com>
2019-09-20 21:52:27 -0500

    dia 6

--- js/map().js
+++ js/map().js
@@ -1,27 +1,77 @@
 //higher order functions are more elegant an simple that will be you look a better programmer
 
-const companies = [
-    {name: "Company One", category: "Finance", start: 1981, end: 2003},
-    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-  ];
+//map()
 
-const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
- 
-var materials = [
-    'Hidrogeno',
-    'Helio',
-    'Litio'
+//el metodo map crea un nuevo array con los resultados de la llamada a la funcion
+//indicada aplicados a cada uno de sus elementos.
+
+/*
+
+    Array.prototipe.map(); 
+
+    El metodo map nos permite devolver un nuevo arreglo partiendo de un arreglo, 
+    dicho así, no mutamos los datos del arreglo original, ahora tenemos
+    un nuevo arreglo con los valores resultantes.
+
+    Pongamos un problema y solucionemos con el metodo map().
+
+    Tenemos  una lista de productos que comprara un usuario y queremos obtener
+    solamente los nombres de los productos a comprar. 
+
+*/
+
+/*
+
+    Para entender un poco más el ejemplo vamos a resolverlo sin el metodo map()
+    para ello vamos a utilizar el metodo for().
+
+*/
+
+const products = [
+    { id: "1", name: "shirt", category: "clothing" },
+    { id: "2", name: "Sports Tennis", category: "accessories" },
+    { id: "3", name: "Casual shoes", category: "footwear" },
+    { id: "4", name: "skirt", category: "clothing" },
+    { id: "5", name: "tie", category: "clothing" }
 ]
 
-var materialsLength = materials.map((material) => {
-    return material.length;
+let nameOfProducts = [];
+
+for (let i = 0; i < products.length; i++) {
+    nameOfProducts.push(products[i].name);
+}
+
+
+console.log(nameOfProducts); // ["shirt","Sports Tennis","Casual shoes","skirt","tie"]
+
+/*
+
+    Ahora resolveremos este ejercicio con el metodo map().
+
+    map() recibe como parametro una funcion la cual recibe tres parametros:
+
+    elemento actual
+    indice del elemento actual
+    arreglo original
+*/
+
+//sintaxis
+    arreglo.map(function(elementoActual, indice, arregloOriginal) {  /* codigo */ });
+
+
+const products = [
+    { id: "1", name: "shirt", category: "clothing" },
+    { id: "2", name: "Sports Tennis", category: "accessories" },
+    { id: "3", name: "Casual shoes", category: "footwear" },
+    { id: "4", name: "skirt", category: "clothing" },
+    { id: "5", name: "tie", category: "clothing" }
+]
+
+let nameOfProducts2 = products.map((product, index, array) => {
+    // Cómo solo queremos los nombres, retornamos "name".
+    return product.name;
 })
 
-console.log(materialsLength);
+console.log(nameOfProducts2); // ["shirt", "Sports Tennis", "Casual shoes", "skirt", "tie"]
+
+
