f184537a222a39b6c383454de9fab43865177e7a
vumg <ab17100222@gmail.com>
2019-09-27 11:34:10 -0500

    dia 12

--- js/functions.js
+++ js/functions.js
@@ -200,7 +200,7 @@ var x = 5;
 */
 
     function myFunction(a, b) {
-        return arguments.length;
+        return arguments.length; //aqui retorna el numero de argumentos recibidos
     }
 
     //si mandamos a llamar la myFunction(2,3) el resultado será 2
@@ -328,7 +328,7 @@ function myFunction(x, y) {
 
   Las funciones de javascript tienen un objecto built-in llamado objeto de los argumentos.
 
-  El objeto argumento contiene un arreglo de los argumentos usados cuando la funcion es invocada.
+  El objeto argumento contiene un arreglo de los argumentos usados cuando la funcion fue invocada.
 
   De esta simple forma puedes usar una funcion para encontrar (por ejemplo) el mayor valor de una lista
   de numeros.
@@ -364,3 +364,230 @@ function sumaTodo(){
 
     return suma;
 }
+
+/*
+  
+    Si una funcion es invocada con muchos argumentos (mas de los declarados) estos argumentos
+    pueden ser accedibles usando el objeto de los argumentos.
+
+    objeto de los argumentos: 
+
+    El objeto de los argumentos contiene un arreglo de todos los argumentos listados cuando
+    la funcion fue invocada.
+
+ */
+
+
+ /*
+ 
+
+    Los argumentos son pasados por valor.
+
+    Los parametros, en una llamada a funcion, son los argumentos de la funcion.
+
+    Los argumentos en javascript son pasados por valor, la funcion solo llega a conocer
+    los valores, no las locaciones de los argumentos.
+
+    Si una funcion cambia el valor de algun argumento, no cambia el valor original del parametro.
+
+    Los cambios a los argumentos no son visibles (reflejados) afuera de la funcion.
+ 
+ */
+
+ /*
+    Los objetos son pasados por referencia.
+
+    En javascriot, la referencia de los objetos son valores.
+
+    Debido a esto, los objetos se comportaran como fueron pasados por referencia:
+
+    Si una funcion cambia una propiedad del objeto, se cambia el valor original.
+
+    Cambios a las propiedades del objeto son reflejadas fuera de la funcion.
+ 
+ */
+
+ /*
+ 
+    Javascript invocacion de funciones
+
+    El codigo dentro de una funcion en javascript sera ejecutado cuando algo lo invoque.
+
+    Invocando una funcion en javascript:
+
+    El codigo dentro de una funcion no es ejecutado cuando es declarada.
+
+    El codigo dentro de una funcion es ejecutado cuando la funcion es invocada.
+
+    Es comun usar el termino "llamar a una funcion" en vez de "invocar a una funcion"
+
+    Tambien es comun decir "llama una funcion", "empieza una funcion", "ejecuta una funcion"
+
+    En este tutorial, usaremos invocar porque una funcion en javascript puede ser invocada sin ser llamada.
+
+ */
+
+ /*
+ 
+    Invocado una funcion como una funcion :V
+
+    EJEMPLO
+
+ */
+
+    function myFunction(a, b) {
+        return a * b;
+    }
+    myFunction(10, 2);           // Will return 20
+
+/* 
+    La funcion de arriba no pertenece a ningun objeto.
+
+    PERO en javascript siempre hay un objeto global por default.
+
+    En html, el objeto global por default es la pagina html por si misma,
+    asi que la funcion de arriba pertenece a la pagina html.
+
+    En un navegador, el objeto de la pagina es la ventana del navegador.
+
+    La funcion de arriba automaticamente se convierte en una window function.
+
+    myFunction() y window.myFunction() es la misma funcion.
+
+*/
+
+    function myFunction(a, b) {
+        return a * b;
+    }
+    window.myFunction(10, 2);    // Will also return 20
+
+/* 
+    Esta es una forma comun de invocar una funcion en javascript, pero no es una buena
+    practica.
+
+    Variables, metodos, o funciones globales pueden crear conflictos con los nombres y 
+    bugs en el objeto global.
+
+*/
+
+/*
+
+    Palabra reservada "this"
+
+    En javascript la palabra reservada this, es el objeto al que le pertenece el codigo actual.
+
+    El valor de this cuando es usado en una funcion, es el objeto al que le pertenece la funcion actual.
+
+    THIS no es una variable, es una palabra clave, no puedes cambiar el valor de this.
+
+*/
+
+/*
+
+    El objeto global.
+
+    Cuando una funcion es llamada sin un objeto dueño, el valor de this se convierte en el objeto global.
+
+    En un navegador web el objeto global es la ventana del navegador.
+
+    Este ejemplo retorna el window object como el valor de this.
+
+*/
+
+    var x = myFunction();            // x will be the window object
+
+    function myFunction() {
+    return this;
+    }
+
+/*
+    Invocar una funcion como una funcion global, causa que el valor de this sea el objeto global. 
+    o sea window object.
+
+    Usar el window object como una variable facilmente puede crashear tu programa.
+
+*/
+
+/*
+
+    Invocar una funcion como un metodo.
+
+    En javascript puedes definir funciones como metodos de un objeto.
+
+    El siguiente ejemplo crea un objeto (myObject), con dos propiedades (firstName, lastName), y un 
+    metodo (fullName).
+
+*/
+
+//ejemplo
+
+var myObject = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  myObject.fullName();         // Will return "John Doe"
+
+/* 
+
+  El metodo fullName es una funcion, la funcion le pertenece al objeto, myObject es el dueño de la funcion.
+
+  La cosa llamada this, es el objeto al que le pertenece el codigo javascript, en este caso el valor
+  de this es myObject. 
+
+  En el ejemplo de abajo cambiamos fullName para que retorne this y veamos a quien le pertence el objeto.
+
+*/
+
+var myObject = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this;
+    }
+  }
+  myObject.fullName();          // Will return [object Object] (the owner object)
+
+/*
+
+  Invocar una funcion como metodo de un objeto, causa que el valor de this sea el objeto en si mismo.
+
+*/
+
+/* 
+
+  Invocar una funcion con una funcion constructor
+
+  Si una invocacion de una funcion es precedida con la palabra "new", es una invocacion constructor.
+
+  Parece que creaste una nueva funcion, pero como las funciones en javascript son objetos creaste un 
+  nuevo objeto.
+
+  EJEMPLO
+
+*/
+
+// This is a function constructor:
+function myFunction(arg1, arg2) {
+    this.firstName = arg1;
+    this.lastName  = arg2;
+  }
+  
+  // This creates a new object
+  var x = new myFunction("John", "Doe");
+  var uriel = new myFunction("Uriel","Mendoza");
+
+  x.firstName;                             // Will return "John"
+  uriel.lastName; //retorna Mendoza
+
+  /* 
+  
+    Una invocacion al constructor crea un nuevo objeto, que hereda 
+    las propiedades y los metodos de su constructor.
+
+    La palabra this en el constructor no tiene un valor.
+    El valor de this sera el nuevo objeto creado cuando la funcion sea invocada.
+  
+  */
